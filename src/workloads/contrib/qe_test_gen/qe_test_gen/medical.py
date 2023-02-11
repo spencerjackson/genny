060c4f6fab970dad4a5a8a93496c083572500d13
Spencer Jackson <spencer.jackson@mongodb.com>
2023-02-11 00:06:38 +0000

    Extract explicit distribution logic

--- src/workloads/contrib/qe_test_gen/qe_test_gen/medical.py
+++ src/workloads/contrib/qe_test_gen/qe_test_gen/medical.py
@@ -1,8 +1,9 @@
-from jinja2 import Environment, PackageLoader, select_autoescape
+from jinja2 import Environment, Template, PackageLoader, select_autoescape
 from faker import Faker
 from random import Random
 import math
 import string
+from abc import ABC, abstractmethod
 
 
 env = Environment(
@@ -16,58 +17,93 @@ env = Environment(
 
 template = env.get_template("medical_datamap.jinja2")
 
-states = {
-    "California": 118000,
-    "Texas": 86030,
-    "Florida": 64280,
-    "New York": 62000,
-    "Pennsylvania": 37500,
-    "Illinois": 38240,
-    "Ohio": 35210,
-    "Georgia": 31970,
-    "North Carolina": 31160,
-    "Michigan": 30000,
-    "New Jersey": 38000,
-    "Virginia": 25760,
-    "Washington": 23000,
-    "Arizona": 21340,
-    "Tennessee": 20620,
-    "Massachusetts": 20980,
-    "Indiana": 20250,
-    "Missouri": 20000,
-    "Maryland": 20000,
-    "Wisconsin": 17590,
-    "Colorado": 17230,
-    "Minnesota": 17030,
-    "South Carolina": 15280,
-    "Alabama": 14990,
-    "Louisiana": 13900,
-    "Kentucky": 13450,
-    "Oregon": 12650,
-    "Oklahoma": 11820,
-    "Connecticut": 10760,
-    "Utah": 9760,
-    "Iowa": 9520,
-    "Nevada": 9270,
-    "Arkansas": 8990,
-    "Mississippi": 8840,
-    "Kansas": 8770,
-    "New Mexico": 6320,
-    "Nebraska": 5850,
-    "Idaho": 5490,
-    "West Virginia": 5350,
-    "Hawaii": 4340,
-    "New Hampshire": 4110,
-    "Maine": 4070,
-    "Montana": 3240,
-    "Rhode Island": 3280,
-    "Delaware": 2950,
-    "South Dakota": 2650,
-    "North Dakota": 2330,
-    "Alaska": 2190,
-    "Vermont": 1920,
-    "Wyoming": 1720,
-}
+
+class Snippet:
+    def __init__(self, template: Template, context):
+        self._template = template
+        self._context = context
+
+    def context(self):
+        return self._context
+
+    def generate(self):
+        return self._template
+
+
+class Distribution(ABC):
+    def __init__(self, field_name: str):
+        self.field_name = field_name
+
+    @abstractmethod
+    def emit_generator():
+        pass
+
+
+class ExplicitDistribution(Distribution):
+    def __init__(self, field_name: str, map: dict[str, int]):
+        super(ExplicitDistribution, self).__init__(field_name)
+        self.map = map
+
+    def emit_generator(self):
+        template = env.get_template("explicit_distribution.jinja2")
+        return Snippet(template, {"field_name": self.field_name, "map": self.map})
+
+
+states = ExplicitDistribution(
+    "states",
+    {
+        "California": 118000,
+        "Texas": 86030,
+        "Florida": 64280,
+        "New York": 62000,
+        "Pennsylvania": 37500,
+        "Illinois": 38240,
+        "Ohio": 35210,
+        "Georgia": 31970,
+        "North Carolina": 31160,
+        "Michigan": 30000,
+        "New Jersey": 38000,
+        "Virginia": 25760,
+        "Washington": 23000,
+        "Arizona": 21340,
+        "Tennessee": 20620,
+        "Massachusetts": 20980,
+        "Indiana": 20250,
+        "Missouri": 20000,
+        "Maryland": 20000,
+        "Wisconsin": 17590,
+        "Colorado": 17230,
+        "Minnesota": 17030,
+        "South Carolina": 15280,
+        "Alabama": 14990,
+        "Louisiana": 13900,
+        "Kentucky": 13450,
+        "Oregon": 12650,
+        "Oklahoma": 11820,
+        "Connecticut": 10760,
+        "Utah": 9760,
+        "Iowa": 9520,
+        "Nevada": 9270,
+        "Arkansas": 8990,
+        "Mississippi": 8840,
+        "Kansas": 8770,
+        "New Mexico": 6320,
+        "Nebraska": 5850,
+        "Idaho": 5490,
+        "West Virginia": 5350,
+        "Hawaii": 4340,
+        "New Hampshire": 4110,
+        "Maine": 4070,
+        "Montana": 3240,
+        "Rhode Island": 3280,
+        "Delaware": 2950,
+        "South Dakota": 2650,
+        "North Dakota": 2330,
+        "Alaska": 2190,
+        "Vermont": 1920,
+        "Wyoming": 1720,
+    },
+)
 
 
 def make_diagnosis_code():
@@ -113,20 +149,24 @@ def make_credit_cards():
     return credit_cards
 
 
-with open("states.txt", "w+") as stateFile:
-    for key, value in states.items():
-        stateFile.write(key + "\n")
+# with open("states.txt", "w+") as stateFile:
+#    for key, value in states.items():
+#        stateFile.write(key + "\n")
 
 diagnosis_code = make_diagnosis_code()
-with open("diagnosis_code.txt", "w+") as diagnosisFile:
-    for key, value in diagnosis_code.items():
-        diagnosisFile.write(key + "\n")
+# with open("diagnosis_code.txt", "w+") as diagnosisFile:
+#    for key, value in diagnosis_code.items():
+#        diagnosisFile.write(key + "\n")
 
 credit_cards = make_credit_cards()
-with open("credit_cards.txt", "w+") as cardFile:
-    for card in credit_cards:
-        cardFile.write(card + "\n")
+# with open("credit_cards.txt", "w+") as cardFile:
+#    for card in credit_cards:
+#        cardFile.write(card + "\n")
 
 
 with open("medical.map", "w+") as mapFile:
-    mapFile.write(template.render({"states": states, "diagnosis_code": diagnosis_code, "credit_cards": credit_cards}))
+    mapFile.write(
+        template.render(
+            {"objFields": [states.emit_generator()], "diagnosis_code": diagnosis_code, "credit_cards": credit_cards}
+        )
+    )
